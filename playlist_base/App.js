6ad898ed87388b861f0ed9f638d783c7d3f04c5a
laislobato <laislobato210109@gmail.com>
2025-08-28 12:21:34 -0300

    asdsadfddfdsgfj

--- playlist_base/App.js
+++ playlist_base/App.js
@@ -1,6 +1,6 @@
-import Reset from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import Ionicons from '@expo/vector-icons';
 
 const { width, height } = Dimensions.get('window');
@@ -9,7 +9,12 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.main}>
-
+        <View style={[styles.imageWrapper, styles.elevation]}>
+          <Image 
+          source={require('./assets/img/gallo.png')}
+          style={styles.musicImage}
+          />
+        </View>
       </View>
       <View style={styles.footer}>
         <View style={styles.iconWrapper}>
@@ -36,7 +41,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#222831',
+  },
+  main : {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  footer: {
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderTopColor: '#393E45',
+    borderTopWidth: 1.5,
+  },
+    iconWrapper: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '80%'
+    },
+    imageWrapper: {
+      width: 340,
+      height: 360,
+      marginVertical: 20,
+    },
+    elevation: {
+      elevation: 5,
+      shadowOffset: {
+        width: 5,
+        height: 5
+      },
+      shadowOpacity: 0.5,
+      shadowRadius: 3.84
+    },
+    musicImage: {
+      width: '100%',
+      height: '100%',
+      borderRadius: 15
+    },
+  }
+);
